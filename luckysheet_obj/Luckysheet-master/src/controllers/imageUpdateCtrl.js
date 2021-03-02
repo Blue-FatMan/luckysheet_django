4996c2a6dea500c4307432ffce5fcdc46af859be
LiuQiao <lq65535@163.com>
2021-03-02 22:57:53 +0800

    优化代码

--- luckysheet_obj/Luckysheet-master/src/controllers/imageUpdateCtrl.js
+++ luckysheet_obj/Luckysheet-master/src/controllers/imageUpdateCtrl.js
@@ -1,9 +1,9 @@
-// 自定义大图片的更新方法
+// 自定义图片的更新方法例如: customImageUpdate("POST", "http://127.0.0.1:8000/luckysheetimageprocess/", d)
 function customImageUpdate(method, url, obj) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
         xhr.open(method, url);
-        xhr.send(JSON.stringify(obj)); // 发送 POST 数据
+        xhr.send(JSON.stringify(obj)); // 发送 POST/GET 数据
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
