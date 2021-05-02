5db8f4e4acf5ccca8118dd6c15cb26f87b7d8a06
LiuQiao <lq65535@163.com>
2021-05-03 03:31:59 +0800

    1、新增更新单个单元格的功能 2、优化添加sheet页的代码

--- luckysheet_obj/apps/lucky_sheet/luckysheet_update.py
+++ luckysheet_obj/apps/lucky_sheet/luckysheet_update.py
@@ -19,9 +19,12 @@ redis_obj = db.RedisDB(settings)
 def update_operate(grid_key, json_obj):
     t = json_obj.get("t")
     lucky_sheet = LuckySheetUpdate(grid_key, json_obj)
+    # 插入新sheet页面
     if "sha" == t:
-        # 插入新sheet页面
         lucky_sheet.insert_sheet()
+    # 单个单元格刷新
+    elif "v" == t:
+        lucky_sheet.single_cell_refresh()
     lucky_sheet.save_redis()
 
 
@@ -42,10 +45,42 @@ class LuckySheetUpdate(object):
         新增sheet页面的时候，需要找到data键,然后添加进去，data是一个列表形式，包含多个sheet页，可以直接append
         :return:
         """
-        # TODO 待判断当前sheet是否在数据库中已经存在
         source_data = self.source_json_data.get("data", list())
-        source_data.append(self.json_obj.get("v"))
+        new_sheet = self.json_obj.get("v")
+        source_sheet_name = [_["name"] for _ in self.source_json_data["data"]]
+        if new_sheet["name"] not in source_sheet_name:
+            source_data.append(new_sheet)
         self.source_json_data["data"] = source_data
+
+    # 单个单元格刷新
+    def single_cell_refresh(self):
+        """
+        单元格更新的时候，celldata和data都要更新，celldata是记录二维数组的，data是直接的数据
+        参考：https://mengshukeji.github.io/LuckysheetDocs/zh/guide/FAQ.html#dist%E6%96%87%E4%BB%B6%E5%A4%B9%E4%B8%8B%E4%B8%BA%E4%BB%80%E4%B9%88%E4%B8%8D%E8%83%BD%E7%9B%B4%E6%8E%A5%E8%BF%90%E8%A1%8C%E9%A1%B9%E7%9B%AE
+        :return:
+        """
+        source_data = self.source_json_data.get("data", list())
+        new_data = self.json_obj.get("v")
+        new_i = self.json_obj.get("i")
+        new_r = self.json_obj.get("r")
+        new_c = self.json_obj.get("c")
+        new_cell_data = dict()
+        new_cell_data["r"] = new_r
+        new_cell_data["c"] = new_c
+        new_cell_data["v"] = new_data
+        source_hasit = False
+        for i, _ in enumerate(source_data):
+            print("_[index], ", _["index"], type(_["index"]))
+            if str(new_i) == str(_["index"]):
+                for j, item in enumerate(_["celldata"]):
+                    source_r, source_c = item["r"], item["c"]
+                    if new_r == source_r and new_c == source_c:
+                        self.source_json_data["data"][i]["celldata"][j]["v"].update(new_data)
+                        source_hasit = True
+                        break
+                if not source_hasit:
+                    self.source_json_data["data"][i]["celldata"].append(new_cell_data)
+                self.source_json_data["data"][i]["data"][int(new_r)][int(new_c)] = new_data
 
     # 保存到redis
     def save_redis(self):
